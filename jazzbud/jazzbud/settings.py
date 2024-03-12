5c83b23a1373fb15b3c39ed87b45ca02bd367820
Zane Kordich <kordich@ualberta.ca>
2024-03-11 19:42:46 -0600

    Added ALLOWED_HOSTS to settings.py

--- jazzbud/jazzbud/settings.py
+++ jazzbud/jazzbud/settings.py
@@ -131,3 +131,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ALLOWED_HOSTS = ['*']
