97eb2b6478f9ba381dbbdec7d390cac8020d8df4
Zane Kordich <43506272+ZaneKordich@users.noreply.github.com>
2024-04-08 21:09:39 -0600

    Update settings.py
    added import render

--- jbud/settings.py
+++ jbud/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 import dj_database_url
 from dotenv import load_dotenv
+import render
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
