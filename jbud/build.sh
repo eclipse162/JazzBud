d4ba3d48cff1cc20b718f05a0de91e848e81a016
Zane Kordich <43506272+ZaneKordich@users.noreply.github.com>
2024-04-08 20:41:00 -0600

    Update build.sh
    fixed

--- jbud/build.sh
+++ jbud/build.sh
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/usr/bin/env bash
 # Exit on error
 set -o errexit
@@ -10,17 +9,4 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
-=======
-#!/usr/bin/env bash
-# Exit on error
-set -o errexit
-
-# Modify this line as needed for your package manager (pip, poetry, etc.)
-pip install -r requirements.txt
-
-# Convert static asset files
-python manage.py collectstatic --no-input
-
-# Apply any outstanding database migrations
->>>>>>> d0fb4ffb5b1e6a03cce8a38e51df9dc5ccfe8759
 python manage.py migrate
