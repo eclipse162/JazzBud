17ccf3d77d0afd8f0ec0a953d0cd38d7ee5ee3d1
Cody Tividad <cody.tividad@gmail.com>
2024-04-14 17:36:26 -0600

    Removed unnecessary imports in settings.py

--- jbud/settings.py
+++ jbud/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = '/' + BASE_DIR/'assets'
+STATIC_ROOT = BASE_DIR/'assets'
 
 if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
