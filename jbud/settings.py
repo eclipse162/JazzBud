2fc8a7b6d198830768b701bbe4b26eaf732ab640
Cody Tividad <cody.tividad@gmail.com>
2024-04-14 17:34:52 -0600

    Removed unnecessary imports in settings.py

--- jbud/settings.py
+++ jbud/settings.py
@@ -13,8 +13,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
-from dotenv import load_dotenv
-import render
+#from dotenv import load_dotenv
+#import render
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
