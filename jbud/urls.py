f796c38026dd5189211c2d2bd946a92060d19efa
Cody Tividad <cody.tividad@gmail.com>
2024-04-22 19:40:04 -0600

    Updated urls.py for testing

--- jbud/urls.py
+++ jbud/urls.py
@@ -16,7 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse('Home page')
+
+def about(request):
+    return HttpResponse('About page')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),
+    path('about/', about),
 ]
