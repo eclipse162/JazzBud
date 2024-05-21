0452b18060c56ffbff0a171410eae9c2e054bd05
Lore Schwartz <lore.a.schwartz@gmail.com>
2024-05-20 22:26:43 -0400

    updated redirect_uri

--- core/views.py
+++ core/views.py
@@ -16,7 +16,7 @@ import requests
 
 CLIENT_ID = "7b37dfd9b4244b60b661ec6013ce3464"
 CLIENT_SECRET = "64c91e959d364004a13c485ae442202a"
-REDIRECT_URI = "http://localhost:8888/callback/"
+REDIRECT_URI = "https://jazzbud.onrender.com/"
 
 
 def index(request):
