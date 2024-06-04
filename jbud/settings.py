2fac5d90ce9069db6f2dd274d8007abba61af7b5
Zane Kordich <43506272+ZaneKordich@users.noreply.github.com>
2024-06-03 20:02:57 -0600

    Update settings.py
    added core/static directory

--- jbud/settings.py
+++ jbud/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR/'assets'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),os.path.join(BASE_DIR, 'core/static')]
 # STATICFILES_DIRS = [
 #     BASE_DIR / 'static',
 #     BASE_DIR / 'core' / 'static',
