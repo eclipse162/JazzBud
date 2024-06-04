95d607cfe0727f8a4e7c0f55bbe96cd275b22b5b
Zane Kordich <43506272+ZaneKordich@users.noreply.github.com>
2024-06-03 20:31:37 -0600

    Update views.py

--- core/views.py
+++ core/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 
-def index(request):
+def login(request):
     return render(request, 'core/login.html')
 
 def about(request):
     return render(request, 'core/about.html')
+
+def home(request):
+    return render(request, 'core/index.html')
