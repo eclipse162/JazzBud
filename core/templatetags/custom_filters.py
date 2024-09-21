ea14aefb478198e74c04b350d30b454c9ee502a7
Lore Schwartz <lore.a.schwartz@gmail.com>
2024-09-20 22:25:10 -0700

    Final custom_filter fix

--- core/templatetags/custom_filters.py
+++ core/templatetags/custom_filters.py
@@ -7,5 +7,5 @@ register = template.Library()
 def ms_to_minutes_seconds(ms):
     seconds = ms // 1000
     minutes = seconds // 60
-    seconds = seconds % 60
+    seconds = int(seconds % 60)
     return f"{minutes}:{seconds:02d}"
