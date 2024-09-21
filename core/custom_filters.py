9356f2b7a633f39ec38af56c8da99a73a5048bf8
Lore Schwartz <lore.a.schwartz@gmail.com>
2024-09-20 22:11:07 -0700

    Added custom filter to handle ms conversions

---
+++ core/custom_filters.py
@@ -0,0 +1,11 @@
+# core/custom_filters.py
+from django import template
+
+register = template.Library()
+
+@register.filter
+def ms_to_minutes_seconds(ms):
+    seconds = ms // 1000
+    minutes = seconds // 60
+    seconds = seconds % 60
+    return f"{minutes}:{seconds:02d}"
