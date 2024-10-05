ce4c8c335dc752b44fa7c52af39dac088ed91ff3
Lore Schwartz <lore.a.schwartz@gmail.com>
2024-10-05 06:24:43 -0700

    Fixed Issues

--- core/urls.py
+++ core/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('home/', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('search/', views.search, name='search'),
-    path('<str:artist_name>/<str:album_id>', views.artist_page, name='artist_page'),
+    path('<str:artist_name>/<str:artist_id>', views.artist_page, name='artist_page'),
     path('<str:artist_name>/<str:album_title>/<str:album_id>', views.album_page, name='album_page')
 ]
