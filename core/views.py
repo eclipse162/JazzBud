c980a3b33b32a79b1fe7909e6e1d5724d1e4a660
Lore Schwartz <lore.a.schwartz@gmail.com>
2025-01-30 16:45:16 -0500

    Another! Whoops!

--- core/views.py
+++ core/views.py
@@ -175,8 +175,8 @@ def track_page(request, artist_name, track_title, track_id):
     spotify_song_id = track['spotify_song_id']
 
     with get_db() as db:
-        get_spotify_song(db, spotify_song_id) is None:
-        create_song(db, spotify_song_id, track['title'], track['artist'], track['artist_id'], track['album'], track['album_id'], track['cover'], track['release_year'], track['track_length'])
+        if get_spotify_song(db, spotify_song_id) is None:
+            create_song(db, spotify_song_id, track['title'], track['artist'], track['artist_id'], track['album'], track['album_id'], track['cover'], track['release_year'], track['track_length'])
 
     html_content = render(request, 'core/edit_partition.html', {'track': track, 'track_title': track_formatted}).content
     response = HttpResponse(html_content)
