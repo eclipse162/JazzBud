6697ef59689631db12e1fe852d8a43ef840d7035
Lore Schwartz <lore.a.schwartz@gmail.com>
2025-04-06 10:27:52 -0400

    urls import updates

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,8 +1,21 @@
-from . import views
-
 from django.urls import path
-from views import home, about, login, search
 from django.contrib.auth import views as auth_views
+
+from .views import (
+    home,
+    about,
+    login,
+    search,
+    artist,
+    album,
+    track,
+    artist_search,
+    instrument_search,
+    save_artist_selection,
+    transfer_playback,
+    playback,
+)
+
 
 
 urlpatterns = [
@@ -11,15 +24,14 @@ urlpatterns = [
     path('about/', about.as_view(), name="about"),
     path('search/', search.as_view(), name='search'),
 
-    path('<str:artist_name>/<str:artist_id>', views.artist, name='artist_page'),
-    path('album/<str:artist_name>/<str:album_title>/<str:album_id>', views.album, name='album_page'),
-    path('track/<str:artist_name>/<str:track_title>/<str:track_id>', views.track, name='track_page'),
+    path('<str:artist_name>/<str:artist_id>', artist, name='artist_page'),
+    path('album/<str:artist_name>/<str:album_title>/<str:album_id>', album, name='album_page'),
+    path('track/<str:artist_name>/<str:track_title>/<str:track_id>', track, name='track_page'),
 
-    path('artist_search/', views.artist_search, name='artist_search'),
-    path('instrument_search/', views.instrument_search, name='instrument_search'),
-    path('save_artist_selection/', views.save_artist_selection, name='save_artist_selection'),
+    path('artist_search/', artist_search, name='artist_search'),
+    path('instrument_search/', instrument_search, name='instrument_search'),
+    path('save_artist_selection/', save_artist_selection, name='save_artist_selection'),
     
-    path("transfer-playback/<str:device_id>/", views.transfer_playback, name="transfer_playback"),
-    path("play/<str:track_uri>/<str:action>/", views.playback, name="play_song"),
-
+    path("transfer-playback/<str:device_id>/", transfer_playback, name="transfer_playback"),
+    path("play/<str:track_uri>/<str:action>/", playback, name="play_song"),
 ]
