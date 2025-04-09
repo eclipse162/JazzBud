0d693a9e8fa6025a9ec5bf8293ac06b8539ce8fe
Lore Schwartz <lore.a.schwartz@gmail.com>
2025-04-09 12:18:14 -0400

    Url routing fix

--- backend/jbud/urls.py
+++ backend/jbud/urls.py
@@ -2,14 +2,19 @@ from django.contrib import admin
 from django.http import HttpResponse
 from django.urls import path, include
 
-from core.views import home, login, about, search
+from core.views import home, login, about, search, artist, album, track, artist_search, instrument_search, save_artist_selection, transfer_playback, playback
 urlpatterns = [
     path('', login.as_view(), name="login"),
     path("home/", home.as_view(), name="home"),
     path("about/", about.as_view(), name="about"),
     path('search/', search.as_view(), name='search'),
+
+    # Music Pages
+    path('track/', track.as_view(), name='track'),
+    path('album/', album.as_view(), name='album'),
+    path('artist/', artist.as_view(), name='artist'),
     
     path('admin/', admin.site.urls),
-    path('core/', include('core.urls')),
+    path('about/', include('core.urls')),
     path('spotify/', include('spotify.urls')),
 ]
