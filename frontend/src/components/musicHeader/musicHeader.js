0e017ddad938a5cb0f4047149f401ebb5e965c33
Lore Schwartz <lore.a.schwartz@gmail.com>
2025-04-11 00:14:13 -0400

    Fixed api import in album header

--- frontend/src/components/musicHeader/musicHeader.js
+++ frontend/src/components/musicHeader/musicHeader.js
@@ -1,5 +1,4 @@
-import { useLocation } from "react-router-dom";
-import { fetchArtist } from "../api.js";
+import { fetchArtist } from "../../api.js";
 import { useNavigate } from "react-router-dom";
 import { customSlugify } from "../utils.js";
 import SpotifyPlayer from "../components/player/spotifyPlayer.js";
